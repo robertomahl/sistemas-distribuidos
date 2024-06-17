d1d95fbd9de5645bb647b0fcfabae552844d707d
Roberto Mahl <roberto@idealogic.com.br>
2024-06-17 19:04:30 -0300

    Fix remote call to room

--- T2/ServerChat.java
+++ T2/ServerChat.java
@@ -1,20 +1,49 @@
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 
-public class ServerChat implements IServerChat {
+public class ServerChat implements IServerChat, Serializable {
 
     private ArrayList<String> roomList;
 
-    private JFrame frame = new JFrame("Server");
+    private DefaultListModel<String> listModel;
+    private JList<String> roomJList;
+    private JFrame frame;
 
     public ServerChat() {
         this.roomList = new ArrayList<>();
+        this.listModel = new DefaultListModel<>();
+        this.roomJList = new JList<>(listModel);
+        this.frame = new JFrame("Server");
 
+        initGUI();
+    }
+
+    private void initGUI() {
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setSize(400, 300);
+        this.frame.setLayout(new BorderLayout());
+
+        JScrollPane scrollPane = new JScrollPane(roomJList);
+        this.frame.add(scrollPane, BorderLayout.CENTER);
+
+        roomJList.addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                String selectedRoom = roomJList.getSelectedValue();
+                if (selectedRoom != null) {
+                    closeRoom(selectedRoom);
+                }
+            }
+        });
+
         this.frame.setVisible(true);
     }
 
@@ -28,12 +57,13 @@ public class ServerChat implements IServerChat {
         if (!roomList.contains(roomName)) {
             RoomChat room = new RoomChat(roomName);
 
+            IRoomChat stub = (IRoomChat) UnicastRemoteObject.exportObject(room, 0);
+
             Registry registry = LocateRegistry.getRegistry("127.0.0.1", 2020);
-            registry.rebind(roomName, room);
+            registry.rebind(roomName, stub);
 
             roomList.add(roomName);
-
-            show();
+            listModel.addElement(roomName);
         }
     }
 
@@ -45,22 +75,11 @@ public class ServerChat implements IServerChat {
             roomChat.closeRoom();
 
             roomList.remove(roomName);
+            listModel.removeElement(roomName);
         } catch (Exception e) {
-            System.err.println("Close room exception: " + e.toString());
+            System.err.println("Close room exception: " + e);
             e.printStackTrace();
         }
-    }
-
-    private void show() {
-        var list = new JList<>(this.roomList.toArray(new String[0]));
-
-        list.setVisible(true);
-
-        list.addListSelectionListener((e) -> {
-            this.closeRoom(list.getSelectedValue());
-        });
-
-        frame.getContentPane().add(list);
     }
 
     public static void main(String[] args) {
@@ -69,15 +88,9 @@ public class ServerChat implements IServerChat {
 
             IServerChat stub = (IServerChat) UnicastRemoteObject.exportObject(server, 0);
             LocateRegistry.createRegistry(2020).rebind("Servidor", stub);
-
-            server.createRoom("Padrao");
-
-            server.show();
         } catch (Exception e) {
             System.err.println("Server exception: " + e.toString());
             e.printStackTrace();
         }
-
     }
-
 }
